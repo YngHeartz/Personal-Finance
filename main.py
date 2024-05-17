6332d62b98f517b3f48ec75c3afa6be9fec70719
Daniel Hill <108755211+YngHeartz@users.noreply.github.com>
2024-05-17 18:05:23 -0400

    Updated variable input types, input verification non zero or below

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # Imports
 import pymongo
-
 # Db connection
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["personal_finance"]
@@ -10,24 +9,29 @@ post_spending = db["spending"]
 # Finance Function
 def Finances():
     # Variables for getting user data
-    month_of_year = input('What is the month you are budgeting for?: ')
-    monthly_budget = input('What is your monthly budget going to be?: ')
-    monthly_earnings = input('What was your monthly earnings?:')
-    monthly_spending = input('What was your monthly spending?:')
-
+    month_of_year = str(input('What is the month you are budgeting for?: '))
+    monthly_budget = int(input('What is your monthly budget going to be?: '))
+    monthly_earnings = int(input('What was your monthly earnings?:'))
+    monthly_spending = int(input('What was your monthly spending?:'))
+    
+    # Checks to see if the user entered a negative number or an invalid number
+    if monthly_budget < 0 or monthly_earnings < 0 or monthly_spending < 0:
+        print('You entered a number less than zero or an invalid number. Your data will not be uploaded.')
+        exit()
+    
     # Calculations for monthly spendings
-    total_left = (int(monthly_earnings) - int(monthly_spending))
+    total_left = (monthly_earnings - monthly_spending)
     print(f'You have ${total_left} left this month')
-    gain_loss = int(monthly_earnings) - int(monthly_budget)
+    gain_loss = int (monthly_earnings - monthly_budget)
 
     # Returns if you made or lost money and how much
-    if int(gain_loss) < int(monthly_spending):
+    if gain_loss < monthly_spending:
         print(f'You made ${gain_loss} dollars this month.')
     else:
         print(f'You lost ${gain_loss} dollars this month.')
     
     # This is the data that will be sent to the database 
-    financial_post = {"month of budget": month_of_year,"monthly budget": int(monthly_budget), "monthly earnings": int(monthly_earnings), "monthly spending": int(monthly_spending), "total left": total_left, "gain or loss": gain_loss}
+    financial_post = {"month of budget": month_of_year,"monthly budget": monthly_budget, "monthly earnings": monthly_earnings, "monthly spending": monthly_spending, "total left": total_left, "gain or loss": gain_loss}
     
     # Sends data to the database
     post_spending.insert_one(financial_post)
@@ -38,5 +42,6 @@ def Finances():
     else:
         print('Finances not Uploaded')
 
+    
 # Calls the function
 Finances()
